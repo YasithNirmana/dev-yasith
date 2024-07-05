b20c669d007d6d319ba633d16ac1fab43aeec4dd
Yasith Nirmana <yaithnirmana99@gmail.com>
2024-07-05 18:41:21 +0800

    Changes

--- js/main.js
+++ js/main.js
@@ -58,16 +58,16 @@
     
     
     // Typed Initiate
-    if ($('.hero .hero-text h2').length == 1) {
-        var typed_strings = $('.hero .hero-text .typed-text').text();
-        var typed = new Typed('.hero .hero-text h2', {
-            strings: typed_strings.split(', '),
-            typeSpeed: 100,
-            backSpeed: 20,
-            smartBackspace: false,
-            loop: true
-        });
-    }
+    // if ($('.hero .hero-text h2').length == 1) {
+    //     var typed_strings = $('.hero .hero-text .typed-text').text();
+    //     var typed = new Typed('.hero .hero-text h2', {
+    //         strings: typed_strings.split(', '),
+    //         typeSpeed: 100,
+    //         backSpeed: 20,
+    //         smartBackspace: false,
+    //         loop: true
+    //     });
+    // }
     
     
     // Skills
